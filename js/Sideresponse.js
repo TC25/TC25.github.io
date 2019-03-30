5fa327b7c0b05c32e0480f46b66cac3ad2283ea7
TC25 <tc.chakraborty@yale.edu>
2019-03-30 16:01:44 -0400

    x

--- js/Sideresponse.js
+++ js/Sideresponse.js
@@ -1,9 +1,9 @@
-if (window.matchMedia("(max-width: 48em)")) {     
+if (window.matchMedia("(min-width: 1000px)")) {     
   //Add your javascript for large screens here
   document.write('<input type="checkbox" class="sidebar-checkbox" id="sidebar-checkbox" checked>');
 }
 else {
   //Add your javascript for small screens here
-  document.write('<input type="checkbox" class="sidebar-checkbox" id="sidebar-checkbox>');
+  document.write('<input type="checkbox" class="sidebar-checkbox" id="sidebar-checkbox" >');
 
 }
